7d753b99af6a6d7cb94f7df5616483b4cda3ae74
Nikodim-monk <lr1975@yandex.ru>
2022-08-01 21:52:21 +0400

    А вдруг прокатит

--- PP_2_1_4_spring-types-of-wiring-master/src/main/java/koschei/models/Duck5.java
+++ PP_2_1_4_spring-types-of-wiring-master/src/main/java/koschei/models/Duck5.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
+    private Egg6 egg = new Egg6();
 
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
     }
 }
--- PP_2_1_4_spring-types-of-wiring-master/src/main/java/koschei/models/Egg6.java
+++ PP_2_1_4_spring-types-of-wiring-master/src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,12 @@
 package koschei.models;
 
-public class Egg6 {
+import org.springframework.stereotype.Component;
 
+@Component
+public class Egg6 {
+    private Needle7 needle = new Needle7();
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
